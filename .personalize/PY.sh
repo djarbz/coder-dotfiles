83c946cc5188dc3d71cc79b98b56d96e62d2a3d7
austin <austin@arbour.me>
2024-04-14 21:57:58 +0000

    Add Jetbrains PyCharm personalization

---
+++ .personalize/PY.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+echo "Applying personalization for PyCharm..."
+
+PYTHONPATH="${PROJECTPATH}"
+export PYTHONPATH
+
+if command -v python3 &> /dev/null; then
+    echo "Installing/Upgrading Python PIP..."
+    python3 -m pip install --upgrade pip
+elif command -v python &> /dev/null; then
+    echo "Installing/Upgrading Python PIP..."
+    python -m pip install --upgrade pip
+fi
+
+if command -v pip3 &> /dev/null; then
+    echo "Installing required Python packages..."
+    pip3 install -q pylint pyyaml
+    pip3 install -q -r "${PYTHONPATH}/requirements.txt" --upgrade
+elif command -v pip &> /dev/null; then
+    echo "Installing required Python packages..."
+    pip install -q pylint pyyaml
+    pip install -q -r "${PYTHONPATH}/requirements.txt" --upgrade
+fi
+
+# TODO: Try to get this working?
+# Complains about PyCharm already running.
+# Wants GUI?
+#
+# PYCHARM_SCRIPT="$(find ${HOME} -name pycharm.sh -type f -executable -print -quit)"
+# if command -v "${PYCHARM_SCRIPT}" &> /dev/null; then
+#     echo "Installing Jetbrains Plugins..."
+#     pylint="com.leinardi.pycharm.pylint"                # https://plugins.jetbrains.com/plugin/11084-pylint
+#     github_actions_toolbar="com.dsoftware.ghtoolbar"    # https://plugins.jetbrains.com/plugin/19347-github-actions-manager
+#     env_file="net.ashald.envfile"                       # https://plugins.jetbrains.com/plugin/7861-envfile
+#     ${PYCHARM_SCRIPT} installPlugins "${pylint}" "${github_actions_toolbar}" "${env_file}"
+# fi

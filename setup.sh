960d9b9b0388e46fb6765c749f0889b52debcdf6
austin <austin@arbour.me>
2024-04-23 13:59:01 +0000

    Don't check for executability

--- setup.sh
+++ setup.sh
@@ -16,11 +16,12 @@ mkdir -p "$HOME/.local/bin/"
 curl -SsL "https://github.com/koalaman/shellcheck/releases/download/stable/shellcheck-stable.linux.x86_64.tar.xz" | tar -xJvf - -C "${BINDIR}/" --strip-components=1 --wildcards '*/shellcheck'
 
 
+# Check if using a Jetbrains IDE
 if [ -n "${JETBRAINS_IDE_ID}" ]; then
   echo "Checking for Jetbrains language specific personalizations..."
   JETBRAINS_PERSONALIZATION_SCRIPT="${SCRIPT_DIR}/.personalize/${JETBRAINS_IDE_ID}.sh"
-  # Check if the IDE ID is set AND the expected script is a file AND the file is executable.
-  if [ -f "${JETBRAINS_PERSONALIZATION_SCRIPT}" ] && [ -x "${JETBRAINS_PERSONALIZATION_SCRIPT}" ]; then
+  # Check if there is a Jetbrains customization script
+  if [ -f "${JETBRAINS_PERSONALIZATION_SCRIPT}" ]; then
     echo "Applying Jetbrains [${JETBRAINS_IDE_ID}] config..."
     source "${JETBRAINS_PERSONALIZATION_SCRIPT}"
   else

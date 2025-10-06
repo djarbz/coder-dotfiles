20bc458f281f536e3968940c556c3ca17504aaa6
default <austin@arbour.me>
2025-10-06 17:59:08 -0500

    Bypass locale and packages

--- setup.sh
+++ setup.sh
@@ -44,26 +44,26 @@ function apt_install {
 }
 export apt_install
 
-echo "Updating Locale..."
-start_time=$SECONDS
-export LANG=en_US.UTF-8
-${SUDO} sed -i -e "s/# $LANG.*/$LANG UTF-8/" /etc/locale.gen
-echo 'locales locales/default_environment_locale select en_US.UTF-8' | ${SUDO} debconf-set-selections
-${SUDO} update-locale LANG=$LANG
-elapsed_seconds=$((SECONDS - start_time))
-echo "Execution time: $elapsed_seconds seconds"
+# echo "Updating Locale..."
+# start_time=$SECONDS
+# export LANG=en_US.UTF-8
+# ${SUDO} sed -i -e "s/# $LANG.*/$LANG UTF-8/" /etc/locale.gen
+# echo 'locales locales/default_environment_locale select en_US.UTF-8' | ${SUDO} debconf-set-selections
+# ${SUDO} update-locale LANG=$LANG
+# elapsed_seconds=$((SECONDS - start_time))
+# echo "Execution time: $elapsed_seconds seconds"
 
 # echo "Pre-Configuring TimeZone..."
 # printf "tzdata tzdata/Areas select US\ntzdata tzdata/Zones/US select Central\n" | ${SUDO} debconf-set-selections
 echo 'tzdata tzdata/Areas select America' | ${SUDO} debconf-set-selections
 echo 'tzdata tzdata/Zones/America select Chicago' | ${SUDO} debconf-set-selections
 
-echo "Installing packages..."
-start_time=$SECONDS
-apt_install apt-utils tzdata git xz-utils
-${SUDO} apt-get -o DPkg::Lock::Timeout=300 install --reinstall -o=Dpkg::Use-Pty=0 --no-install-recommends -yqq locales
-elapsed_seconds=$((SECONDS - start_time))
-echo "Execution time: $elapsed_seconds seconds"
+# echo "Installing packages..."
+# start_time=$SECONDS
+# apt_install apt-utils tzdata git xz-utils
+# ${SUDO} apt-get -o DPkg::Lock::Timeout=300 install --reinstall -o=Dpkg::Use-Pty=0 --no-install-recommends -yqq locales
+# elapsed_seconds=$((SECONDS - start_time))
+# echo "Execution time: $elapsed_seconds seconds"
 
 BINDIR="$HOME/.local/bin"
 export BINDIR
@@ -76,19 +76,23 @@ fi
 export PATH
 
 ## JQ
-echo "Installing JQ..."
-start_time=$SECONDS
-JQ_RELEASE_VERSION=$(git -c 'versionsort.suffix=-' ls-remote --tags --sort='v:refname' https://github.com/jqlang/jq.git | grep -v rc | tail --lines=1 | cut --delimiter='/' --fields=3)
-curl -SsL "https://github.com/jqlang/jq/releases/download/${JQ_RELEASE_VERSION}/jq-linux-amd64" -o "${BINDIR}/jq"
-elapsed_seconds=$((SECONDS - start_time))
-echo "Execution time: $elapsed_seconds seconds"
+if command -v jq &> /dev/null; then
+  echo "Installing JQ..."
+  start_time=$SECONDS
+  JQ_RELEASE_VERSION=$(git -c 'versionsort.suffix=-' ls-remote --tags --sort='v:refname' https://github.com/jqlang/jq.git | grep -v rc | tail --lines=1 | cut --delimiter='/' --fields=3)
+  curl -SsL "https://github.com/jqlang/jq/releases/download/${JQ_RELEASE_VERSION}/jq-linux-amd64" -o "${BINDIR}/jq"
+  elapsed_seconds=$((SECONDS - start_time))
+  echo "Execution time: $elapsed_seconds seconds"
+fi
 
-
-echo "Installing Shellcheck..."
-start_time=$SECONDS
-curl -SsL "https://github.com/koalaman/shellcheck/releases/download/stable/shellcheck-stable.linux.x86_64.tar.xz" | tar -xJvf - -C "${BINDIR}/" --strip-components=1 --wildcards '*/shellcheck'
-elapsed_seconds=$((SECONDS - start_time))
-echo "Execution time: $elapsed_seconds seconds"
+## Shellcheck
+if command -v shellcheck &> /dev/null; then
+  echo "Installing Shellcheck..."
+  start_time=$SECONDS
+  curl -SsL "https://github.com/koalaman/shellcheck/releases/download/stable/shellcheck-stable.linux.x86_64.tar.xz" | tar -xJvf - -C "${BINDIR}/" --strip-components=1 --wildcards '*/shellcheck'
+  elapsed_seconds=$((SECONDS - start_time))
+  echo "Execution time: $elapsed_seconds seconds"
+fi
 
 # Update and apply changes to workspace repository
 # if [ -v PROJECT_DIRECTORY ]; then
